873f0690f6e82a0d5446af1dc4f7f52f4583ddcc
Liang <zhoul@mit.edu>
2015-11-11 04:21:45 -0500

    my stuff

--- matlab/gradDescent.m
+++ matlab/gradDescent.m
@@ -1,29 +1,25 @@
-function [w1, w1_0, w2, w2_0] = gradDescent(lossFunc, grad, X, Y, w1, w1_0, w2, w2_0, lambda, step, maxIter, threshold)
+function [w1, w2] = gradDescent(lossFunc, grad, X, Y, w1, w2, lambda, step, maxIter, threshold)
+Y = Y';
 iterations = 0;
 exampleCount = size(Y,1);
 ow1 = zeros(size(w1));
-ow1_0 = zeros(size(w1_0));
 ow2 = zeros(size(w2));
-ow2_0 = zeros(size(w2_0));
-while abs(lossFunc(w1, w1_0, w2, w2_0, X, Y) - lossFunc(ow1, ow1_0, ow2, ow2_0, X, Y)) > threshold && iterations < maxIter
+while abs(lossFunc(w1, w2, X, Y) - lossFunc(ow1, ow2, X, Y)) > threshold && iterations < maxIter
     ow1 = w1;
-    ow1_0 = w1_0;
     ow2 = w2;
-    ow2_0 = w2_0;
-    exampleNum = mod(iterations, exampleCount) + 1; % may need to change this to random selection
+    exampleNum = ceil(rand*exampleCount);
+    %exampleNum = mod(iterations, exampleCount) + 1; % may need to change this to random selection
     xexample = X(exampleNum, :);
     yexample = Y(exampleNum, :);
-    [w1, w1_0, w2, w2_0] = gradDescentStep(grad, xexample, yexample, ow1, ow1_0, ow2, ow2_0, step, lambda);
+    [w1, w2] = gradDescentStep(grad, xexample, yexample, ow1, ow2, step, lambda);
+    iterations = iterations + 1;
+    lossFunc(w1,w2,X,Y)
 end
 end
 
 
-function [w1, w1_0, w2, w2_0] = gradDescentStep(grad, x, y, w1, w1_0, w2, w2_0, step, lambda)
-[gradW] = grad(w1, w2, w1_0, w2_0, x, y, lambda);
-w1 = gradW(1,:);
-w2 = gradW(2,:);
-w1 = w1 + step*gradw1;
-w1_0 = w1_0 + step*gradw1_0;
-w2 = w2 + step*gradw2;
-w2_0 = w2_0 + step*gradw2_0;
+function [w1, w2] = gradDescentStep(grad, x, y, w1, w2, step, lambda)
+[gradw1, gradw2] = grad(w1, w2, x, y, lambda);
+w1 = w1 - step*gradw1;
+w2 = w2 - step*gradw2;
 end
--- matlab/lossFunc.m
+++ matlab/lossFunc.m
@@ -1,20 +1,20 @@
-function [ out ] = lossFunc( w1, w1_0, w2, w2_0, X, Y )
+function [ out ] = lossFunc( w1, w2, X, Y )
+Y = Y';
 
 out = 0;
-Kval = max(Y);
+Kval = size(Y,1);
 
 for i = 1:size(X,1)
-    y = zeros(Kval);
-    y(Y(i,:)) = 1;
+    y = Y(:,i);
     x = X(i,:);
     for kthis = 1:Kval
-        hk = h(x, w1, w1_0, w2, w2_0);
-        out = out - y(kthis)*log(hk) - (1-y(kthis))*(1-log(hk));
+        hk = h(x, w1, w2);
+        out = out - y(kthis).*log(hk(kthis))' - (1-y(kthis)).*(1-log(hk(kthis)))';
     end
 end
 end
 
-function [ Z, a1, a2 ] = h( X, W1, W1_0, W2, W2_0 )
+function [ Z, z, a1, a2 ] = h( X, W1, W2 )
 %h Implements a neural network
 %   X: input layer, d x 1
 %   W1: weights 1, m x d
@@ -23,7 +23,7 @@ function [ Z, a1, a2 ] = h( X, W1, W1_0, W2, W2_0 )
 %   W2_0: weight 2 bias, k x 1
 
 d = length(X);
-m = length(W1_0);
+m = size(W1,1);
 z = zeros(m, 1);
 a1 = zeros(m,1);
 for j = 1:m
@@ -31,20 +31,20 @@ for j = 1:m
     for i = 1:d
         a = a + W1(j, i) * X(i);
     end
-    a = a + W1_0(j);
     a1(j) = a;
     z(j) = sigmoid(a);
 end
+z = [z; 1];
 
-K = length(W2_0);
+K = size(W2,1);
 Z = zeros(K, 1);
 a2 = zeros(K, 1);
+m = m + 1;
 for k = 1:K
     a = 0;
     for j = 1:m
         a = a + W2(k, j) * z(j);
     end
-    a = a + W2_0(k);
     a2(k) = a;
     Z(k) = sigmoid(a);
 end
@@ -52,5 +52,5 @@ end
 end
 
 function [ out ] = sigmoid( z )
-out = 1 / (1 + exp(-z));
+out = (1+exp(-z)).^(-1);
 end
